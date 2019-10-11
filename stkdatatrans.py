90d54adb3a72fc8bcfebbfd3ae6921c2f2127061
James <jingzl@hotmail.com>
2019-10-11 11:24:01 +0800

    add log info with datetime

--- stkdatatrans.py
+++ stkdatatrans.py
@@ -5,6 +5,7 @@ Created on Fri Oct  4 22:12:39 2019
 @author: jingzl
 """
 import os
+import time
 from func import cons as ct
 from func.configparser import ConfigParser
 from func.datatrans import DataTrans
@@ -12,6 +13,8 @@ from func.datatrans import DataTrans
 
 if __name__ == '__main__':
     
+    print( "{}".format( '-'*50 ) )
+    print("[{0}]--update stockdata db begin...".format(time.strftime("%Y-%m-%d %H:%M:%S")))
     config_file = os.path.dirname( os.path.realpath(__file__)) + ct.CONFIG_FNAME
     config = ConfigParser()
     if config.parse( config_file ):
@@ -20,6 +23,7 @@ if __name__ == '__main__':
         datatrans.trans()
     else:
         print( "解析配置文件失败，请检查配置文件！" )
+    print("[{0}]--update stockdata db end".format(time.strftime("%Y-%m-%d %H:%M:%S")))
     
 
 
