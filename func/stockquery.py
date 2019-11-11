98d301b05426383adec9349925409bb63e33709c
jingzl <jingzl@hotmail.com>
2019-11-11 10:04:27 +0800

    add try except when call tushare api

--- func/stockquery.py
+++ func/stockquery.py
@@ -7,6 +7,8 @@ Created on Tue Sep 10 21:40:42 2019
 parse stock data from tushare interface
 """
 import datetime
+import pandas as pd
+import numpy as np
 import tushare as ts
 
 TUSHARE_TOKEN = '8af63d3d7f7ec3bc0e511a85b13d0cf01c57dd310bc1f8ed4b902ad3'
@@ -17,19 +19,26 @@ class StockQuery( object ):
         pass
     
     def getStockBasicData( self ):
-        
-        # 获取所有的股票列表信息
-        pro = ts.pro_api( token=TUSHARE_TOKEN )
-        # 查询当前所有正常上市交易的股票列表 , symbol, name
-        data = pro.query( 'stock_basic', exchange='', list_status='L', 
-                         fields='ts_code,symbol,name,area,industry,list_status,list_date')
-        #print( data )
-        #print( data.ix[0:3] )
-        #print( data.columns )
-        #print( data.ix[ 0:3, 1 ] )
-        
-        #dl = data.ix[ :, 0 ].values
-        return data
+        try:
+            # 获取所有的股票列表信息
+            pro = ts.pro_api( token=TUSHARE_TOKEN )
+            # 查询当前所有正常上市交易的股票列表 , symbol, name
+            data = pro.query( 'stock_basic', exchange='', list_status='L', 
+                             fields='ts_code,symbol,name,area,industry,list_status,list_date')
+            #print( data )
+            #print( data.ix[0:3] )
+            #print( data.columns )
+            #print( data.ix[ 0:3, 1 ] )
+            
+            #dl = data.ix[ :, 0 ].values
+            return data
+        except:
+            a = []
+            df = pd.DataFrame( a, columns=['ts_code','symbol','name',
+                                           'area','industry','list_status',
+                                           'list_date'],
+                             index=np.arange(len(a)))
+            return df
         
 
     def getSingleStockData( self, ts_code, length ):
@@ -47,23 +56,33 @@ class StockQuery( object ):
         #print( length )
         #print( start_date )
         #print( end_date )
-        
-        df = pro.query( 'daily', ts_code=ts_code, start_date=start_date,
+        try:
+            df = pro.query( 'daily', ts_code=ts_code, start_date=start_date,
                        end_date=end_date, fields='trade_date,close')
-        dl = df.ix[:,1].values.tolist()
-        dl.reverse()
-        if len(dl) < length:
+            dl = df.ix[:,1].values.tolist()
+            dl.reverse()
+            if len(dl) < length:
+                return []
+            else:
+                return dl[0:length]
+        except:
             return []
-        else:
-            return dl[0:length]
 
     def getStockDailyData( self, ts_code, start_date, end_date ):
-        pro = ts.pro_api( token=TUSHARE_TOKEN )
-        df = pro.query( 'daily', ts_code=ts_code, start_date=start_date,
+        try:
+            pro = ts.pro_api( token=TUSHARE_TOKEN )
+            df = pro.query( 'daily', ts_code=ts_code, start_date=start_date,
                        end_date=end_date, fields='ts_code,trade_date,open,\
                        high,low,close,pre_close,change,pct_chg,vol,amount')
-        #print( df )
-        return df
+            #print( df )
+            return df
+        except:
+            a = []
+            df = pd.DataFrame( a, columns=['ts_code','trade_date','open',
+                                           'high','low','close','pre_close',
+                                           'change','pct_chg','vol','amount'],
+                             index=np.arange(len(a)))
+            return df
 
     def getTradeCal( self, start_date, end_date ):
         return True
